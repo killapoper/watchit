a2f4e466993bc1d3a32504d72d355a8277a43bc4
killapoper <dawedenkold@gmail.com>
2025-06-15 22:22:16 +0300

    Обновил сайт: 0.7.3

--- parser.py
+++ parser.py
@@ -1,30 +1,38 @@
+
 import requests
 from bs4 import BeautifulSoup
 
+headers = {
+    'User-Agent': 'Mozilla/5.0'
+}
+
 def search_hdrezka(query):
-    headers = {
-        'User-Agent': 'Mozilla/5.0',
-    }
     search_url = f'https://hdrezka.ag/search/?do=search&subaction=search&q={query}'
     r = requests.get(search_url, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     results = []
-    for item in soup.select('.b-content__inline_item')[:5]:  # первые 5 результатов
-        title = item.select_one('.b-content__inline_item-link').text.strip()
-        href = item.select_one('.b-content__inline_item-link')['href']
-        results.append({'title': title, 'url': href})
-    return results
+    for item in soup.select('.b-content__inline_item'):
+        try:
+            title = item.select_one('.b-content__inline_item-link').text.strip()
+            url = item.select_one('.b-content__inline_item-link')['href']
+            image = item.select_one('img')['src']
+            description = item.select_one('.b-content__inline_item-link + div').text.strip()
+            results.append({
+                'title': title,
+                'url': url,
+                'image': image,
+                'description': description
+            })
+        except:
+            continue
+
+    return results[:10]  # limit to 10 results
 
 def get_video_links(movie_page_url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0',
-    }
     r = requests.get(movie_page_url, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
-
     iframe = soup.find('iframe')
     if iframe:
-        return iframe['src']  # ссылка на плеер
-
+        return iframe['src']
     return None
--- server.py
+++ server.py
@@ -93,26 +93,68 @@
 #     video_url = get_video_links(url)
 #
 #     return render_template('film.html', title=title, video_url=video_url)
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
+# from parser import search_hdrezka, get_video_links
+#
+# app = Flask(__name__, static_folder='static', template_folder='templates')
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+#
+# @app.route('/search')
+# def search():
+#     query = request.args.get('q', '')
+#     if not query:
+#         return render_template('index.html', results=[])
+#
+#     results = search_hdrezka(query)
+#     return render_template('index.html', results=results, query=query)
+#
+#
+# @app.route('/watch')
+# def watch():
+#     url = request.args.get('url')
+#     if not url:
+#         return "Нет ссылки на фильм", 400
+#
+#     video_link = get_video_links(url)
+#     if not video_link:
+#         return "Видео не найдено", 404
+#
+#     return render_template('watch.html', video_link=video_link)
+from flask import Flask, render_template, request, jsonify
 from parser import search_hdrezka, get_video_links
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    # Фильмы по умолчанию
+    default_movies = [
+        {"title": "Интерстеллар", "poster": "/static/img/interstellar.jpg", "url": "/watch?title=Интерстеллар"},
+        {"title": "Начало", "poster": "/static/img/inception.jpg", "url": "/watch?title=Начало"},
+        {"title": "Матрица", "poster": "/static/img/matrix.jpg", "url": "/watch?title=Матрица"},
+        {"title": "Остров проклятых", "poster": "/static/img/shutter_island.jpg", "url": "/watch?title=Остров проклятых"},
+        {"title": "Аватар", "poster": "/static/img/avatar.jpg", "url": "/watch?title=Аватар"},
+        {"title": "Титаник", "poster": "/static/img/titanic.jpg", "url": "/watch?title=Титаник"},
+        {"title": "Темный рыцарь", "poster": "/static/img/dark_knight.jpg", "url": "/watch?title=Темный рыцарь"},
+        {"title": "Дюна", "poster": "/static/img/dune.jpg", "url": "/watch?title=Дюна"},
+        {"title": "Гравитация", "poster": "/static/img/gravity.jpg", "url": "/watch?title=Гравитация"},
+        {"title": "Хоббит", "poster": "/static/img/hobbit.jpg", "url": "/watch?title=Хоббит"},
+    ]
+    return render_template("index.html", movies=default_movies)
 
 @app.route('/search')
 def search():
     query = request.args.get('q', '')
     if not query:
-        return render_template('index.html', results=[])
+        return jsonify([])
 
     results = search_hdrezka(query)
-    return render_template('index.html', results=results, query=query)
-
+    return jsonify(results)
 
 @app.route('/watch')
 def watch():
@@ -124,4 +166,4 @@ def watch():
     if not video_link:
         return "Видео не найдено", 404
 
-    return render_template('watch.html', video_link=video_link)
+    return render_template('film.html', video_link=video_link)
