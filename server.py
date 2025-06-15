fb6f845eec64e20551a6031c50d1cdd088260503
killapoper <dawedenkold@gmail.com>
2025-06-15 23:12:59 +0300

    Обновил сайт: 0.9.1

--- server.py
+++ server.py
@@ -1,20 +1,18 @@
 from flask import Flask, render_template, request
-from parser import search_hdrezka, get_video_links
+from parser import search_hdrezka
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', results=None, query=None)
+    movies = []
+    if request.method == 'POST':
+        query = request.form['query']
+        try:
+            movies = search_hdrezka(query)
+        except Exception as e:
+            print(f'Ошибка при поиске: {e}')
+    return render_template('index.html', movies=movies)
 
-@app.route('/search')
-def search():
-    query = request.args.get('q', '')
-    results = search_hdrezka(query) if query else []
-    return render_template('index.html', results=results, query=query)
-
-@app.route('/watch')
-def watch():
-    url = request.args.get('url', '')
-    video_link = get_video_links(url)
-    return render_template('watch.html', video_link=video_link)
+if __name__ == '__main__':
+    app.run(debug=True)
